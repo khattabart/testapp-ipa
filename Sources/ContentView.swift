e8afa1f2033857504efd6109bfd3a0bc9dba9215
khattabart <ka2190984@gmail.com>
2026-09-22 06:30:00 +0300

    Create ContentView.swift

---
+++ Sources/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, Test App!")
+        }
+        .padding()
+    }
+}
